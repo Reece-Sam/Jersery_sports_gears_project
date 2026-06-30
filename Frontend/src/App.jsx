92e8e60688246270db14c96e87e54f2142de416a
Reece-Sam <bonisamkelekoh@gmail.com>
2026-06-30 18:06:21 +0100

    Add Footer and Correct fix product-card error

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Home />
+      <Footer />
     </BrowserRouter>
   );
 }
---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import "../css/footer.css";
+
+function Footer() {
+    return (
+      <footer className="footer">
+        <p>© 2026 Jersey Sport Gears aka The League. All Rights Reserved.</p>
+      </footer>
+    );
+}
+
+export default Footer;
